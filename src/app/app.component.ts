e2d81ff36ca442323cb83b7639c802c0b270f8b9
PavloMylkus <ppavlo82@gmail.com>
2022-11-28 11:02:42 +0200

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { IProduct } from './models/product';
+import { products as data } from './data/products';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-test-app';
+	title = 'angular test app';
+
+	products: IProduct[] = data
 }
